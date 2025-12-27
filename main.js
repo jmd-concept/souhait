71531670e8da5fdfe0e56f50c3b80f399de7c529
jmd-concept <jmdconcept3@gmail.com>
2025-12-27 21:16:46 +0100

    8th Commit ajustement étoile et boule blue

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ setTimeout(() => {
             boxHidden.classList.add("flex");
         }, 8000);
     }
-}, 5000);
+}, 5 * 1000);
 
 const formulaire = document.getElementById('formulaire');
 const nom = document.getElementById('nom');
